77645e487d57d1aa8f060b7202d8add10e2dcb67
cacasasasa <128118198+cacasasasa@users.noreply.github.com>
2023-03-23 10:31:19 -0500

    Created class example.

---
+++ class_2_testing.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Prompt the user for their name
+name = input("What is your name? ")
+
+# Greet the user
+print(f"Hello, {name}!")
